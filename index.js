14ca46ac0ca10b9da9a99eae5af5919aaad3cfef
Nathaniel Gibbons <ngibbons333@gmail.com>
2018-07-25 01:55:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+/*function tryout(name){
+  console.log(`Hello, ${name}`)
+}
+
+*/
+function tryout(greeting, name){
+  //console.log(`${greeting}, ${name}`)
+  return `${greeting}, ${name}`;
+}
+tryout('Fuck you','Nathaniel');
+console.log('**complete**');
